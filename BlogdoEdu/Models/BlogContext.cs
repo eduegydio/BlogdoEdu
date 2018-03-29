e5b8dfb16f91e6ee45060fcb1f8ad920f6b111a0
EDUARDO EGYDIO DA SILVA <eduardo.esilva7@senacsp.edu.br>
2018-03-29 09:33:34 -0300

    Adicionar arquivos de projeto.

---
+++ BlogdoEdu/Models/BlogContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace BlogdoEdu.Models
+{
+    public class BlogContext: DbContext
+    
+    {
+        public DbSet<Post> Posts { get; set; }
+
+        public BlogContext() : base("DefaultConnection")
+        {
+
+        }
+
+    }
+}
